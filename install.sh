5c930bf2429d4e66a4f0ee15d1379e26b613c3d2
David Mohammed <fossfreedom@users.noreply.github.com>
2015-01-06 21:09:57 +0000

    use $HOME rather than $USER for those users who's home is not home

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ RB=${RB:=true}
 
 SCRIPT_NAME=`basename "$0"`
 SCRIPT_PATH=${0%`basename "$0"`}
-PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/rhythmweb/"
+PLUGIN_PATH="${HOME}/.local/share/rhythmbox/plugins/rhythmweb/"
 GLIB_SCHEME="org.gnome.rhythmbox.plugins.rhythmweb.gschema.xml"
 SCHEMA_FOLDER=""
 GLIB_DIR="/usr/share/glib-2.0/schemas/"
